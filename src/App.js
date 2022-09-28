dd27227a0997143a36b23c98914836303049ab81
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-09-29 02:44:21 +0600

    fake data created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [drills, setDrills] = useState([]);
+
+  useEffect(()=>{
+    fetch ('swim.json')
+    .then(res => res.json())
+    .then(data => setDrills(data))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          drills.map(drill => <img src ={drill.picture}></img>)
+        }
     </div>
   );
 }
